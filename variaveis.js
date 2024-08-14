08a94d746be5ae9b421d83393e23518564eed105
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-08-14 10:32:05 -0300

    Add files via upload

--- variaveis.js
+++ variaveis.js
@@ -16,3 +16,43 @@ console.log(multiplicacao);
 
 const divisao = meuNumero / numeroDois;
 console.log(divisao);
+
+//Float (numero decimal)
+const numeroTres = 3.3;
+const numeroQuatro = .5; //mesma coisa que 0.5
+
+const operacaoM = numeroTres * numeroQuatro;
+console.log(operacaoM);
+
+// NaN not a number
+
+const operacaoN = 'a' * meuNumero;
+console.log(operacaoN);
+
+const novaOperacao = "a" + meuNumero;
+console.log(novaOperacao);
+
+//string (texto)
+
+const meuNome = 'Gustavo'
+const frase = "Meu nome é: "
+const IssoNaoEUmNumero = "50"
+
+const minhaFrase = frase + meuNome;
+console.log(minhaFrase);
+
+const operacaoO = IssoNaoEUmNumero + meuNumero;
+console.log(operacaoO);
+
+// Boolean 
+// True -> verdadeiro;
+// False -> falso;
+
+const comparacao = 6 === meuNumero;
+console.log(comparacao);
+
+const comparacaoII = "6" === meuNumero;
+console.log(comparacaoII);
+
+const comparacaoIII = "6" == meuNumero;
+console.log(comparacaoIII);

aecd3f43ae5f7d43e89df88053d24b2a25f84641
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-08-14 09:28:20 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+// Variáveis 
+// tipo Number
+
+const meuNumero = 6
+const numeroUm = 10
+const numeroDois = 11
+
+const soma = numeroDois + numeroUm;
+console.log(soma);
+
+const subtracao = meuNumero - numeroDois;
+console.log(subtracao);
+
+const multiplicacao = numeroDois * numeroUm;
+console.log(multiplicacao);
+
+const divisao = meuNumero / numeroDois;
+console.log(divisao);
